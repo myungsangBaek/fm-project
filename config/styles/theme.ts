9e0d86afb56a60ac45705acb8754d6d70fa234fd
myungsangBaek <bms6168@naver.com>
2023-03-02 16:49:21 +0900

    [FEATURE] Setting svg config

---
+++ config/styles/theme.ts
@@ -0,0 +1,11 @@
+const ThemeConfig = {
+  colors: {
+    gray: "#E9E8E8",
+
+    main: "#E10078",
+
+    white: "#FFFFFF",
+  },
+};
+
+export default ThemeConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import ThemeConfig from "@/config/styles/theme";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ThemeConfig}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
