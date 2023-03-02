a8eae37404a21a2c2af980e1646cb00dcda05fe1
myungsangBaek <bms6168@naver.com>
2023-03-02 16:56:46 +0900

    [FEATURE] Set reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import ThemeConfig from "@/config/styles/theme";
-import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+import { Reset } from "styled-reset";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={ThemeConfig}>
+      <Reset />
       <Component {...pageProps} />
     </ThemeProvider>
   );
