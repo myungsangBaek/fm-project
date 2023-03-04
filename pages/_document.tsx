ba6bf37203e8f53629f4e570c3fc5ab0141706f5
myungsangBaek <bms6168@naver.com>
2023-03-04 09:58:29 +0900

    [FEATURE] Generate og tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/fm-favicon.png" />
+        <meta property="og:title" content="FM TODO"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:description" content="FM Taskly App"></meta>
+        <meta property="og:url" content="https://fm-project.vercel.app/"></meta>
+        <meta property="og:image" content="/img_todo.jpg"></meta>
       </Head>
       <body>
         <Main />
